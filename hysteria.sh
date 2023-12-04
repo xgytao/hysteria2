eae1d0566935b1959fe5337fc5ad636a616cf24f
xgytao <xgytao@gmail.com>
2023-12-04 12:40:28 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -502,7 +502,7 @@ showconf(){
 }
 
 update_core(){
-    wget -N https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/install_server.sh
+    wget -N https://get.hy2.sh/install_server.sh
     bash install_server.sh
     
     rm -f install_server.sh
